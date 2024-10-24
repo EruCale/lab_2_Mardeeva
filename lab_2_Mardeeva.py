3ba4dd2781e51c25a835c94694e6503cac8ef239
Диана <eru@>
2024-10-24 16:36:23 +0300

    Task 1 from 1 to 6 items was completed

---
+++ lab_2_Mardeeva.py
@@ -0,0 +1,38 @@
+print("Programming ***","Essential ***","in ...","Python")
+
+print("     *   ")
+print("    * * ")
+print("   *   * " )
+print("  *     * " )
+print(" ***   *** ")
+print("   *   * ")
+print("   *   * ")
+print("   ***** ")
+
+print("I'm student")
+
+
+print("I`m\nlearning\nPython")
+
+
+# Вісімкове число
+octal_number = '500'
+
+# Перетворення в десяткову систему
+decimal_number = int(octal_number, 8)
+
+# Виведення результату
+print(decimal_number)  # Виведе: 320
+
+
+# Шістнадцяткове число
+hex_number = '777'
+
+# Конвертація у десяткову систему
+decimal_number = int(hex_number, 16)
+
+# Виведення результату
+print(decimal_number)   # Виведе: 1911
+
+
+
